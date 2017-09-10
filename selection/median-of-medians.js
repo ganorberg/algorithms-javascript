c0217611e2a48d0b490d27514551eaa7b9e1bd4f
George Norberg <george.norberg@gmail.com>
2017-09-10 13:16:09 -0700

    Generalize to accept variable number of medians

--- selection/median-of-medians.js
+++ selection/median-of-medians.js
@@ -1,9 +1,14 @@
 // could be used in quick select to approximate true median for optimized pivot
-function medianOf3(arr, lo, mid, hi) {
-  return [arr[lo], arr[mid], arr[hi]].sort((a, b) => a < b)[1];
+function medianOfMedians(arr, ...indices) {
+  const medianIndex = Math.floor(indices.length / 2);
+  const values = indices
+    .map(idx => arr[idx])
+    .sort((a, b) => a > b);
+  
+  return values[medianIndex];
 }
 
 const array = [5, 3, 7, 2, 1];
-const letters = ['A', 'E', 'T', 'C', 'J'];
-console.log(medianOf3(array, 0, 2, 4)); // 5
-console.log(medianOf3(letters, 0, 2, 4)); // J
+const letters = ['A', 'E', 'T', 'C', 'J', 'H'];
+console.log(medianOfMedians(array, 0, 2, 4)); // 571 -> 157 -> 5
+console.log(medianOfMedians(letters, 0, 2, 4, 5)); // ATJH -> AHJT -> J
