6d75a7c060dadbaeb0baaacef66f22b587e2a26a
George Norberg <george.norberg@gmail.com>
2017-11-21 23:00:05 -0800

    Explain why combinations slow after length 20 input

--- combinatorics/power-set-bottom-up.js
+++ combinatorics/power-set-bottom-up.js
@@ -3,8 +3,9 @@
  * Assumptions:
  * - include empty set
  * - input data type is array
- * - input length does not exceed 20 since 2^20 requires ~1 second to compute
- *   and each additional unit of length will double that already slow time
+ * - gets really slow after length 20 because 2^20 = 1M operations, and typical
+ *   CPU operates at roughly 1M ops/sec. Each additional unit of length will
+ *   continue to double the time, so 23 would take ~8 seconds, for example.
  *
  * Time complexity: O(2^N)
  * Space complexity: O(2^N)
@@ -28,6 +29,6 @@ function findPowerSet(arr) {
 }
 
 const arr0 = [1, 2, 3];
-const arr1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't'];  // gets really slow after length 20 because 2^20 takes ~1 second
+const arr1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't'];  // gets really slow after length 20 because 2^20 = 1M which takes ~1 second on typical GHz-operating CPU
 console.log(findPowerSet(arr0));
 console.log(findPowerSet(arr1));
