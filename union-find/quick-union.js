37f9bf6752709075dba5b25207d4f00250e09b4e
George Norberg <george.norberg@gmail.com>
2017-11-28 21:16:57 -0800

    Clean up comments

--- union-find/quick-union.js
+++ union-find/quick-union.js
@@ -1,6 +1,6 @@
-/* Next strategy: create trees of components. Each tree will have a root to
+/* Next strategy: Create trees of components. Each tree will have a root to
  * identify that set. Union function will take first tree and attach to
- * root of second tree (assuming unconnected) by changing first * value to second.
+ * root of second tree (assuming unconnected) by changing first value to second.
  *
  * This turned out mangled and gross in terms of modularity... cleaned up in
  * quick-union-root file by abstracting root search and connect logic.
@@ -10,17 +10,17 @@ const values = [0, 1, 2, 3, 4];
 function union(a, b) {
   const [connected, rootA, rootB] = find(a, b);
   if (!connected) {
-    // assign first root to second root
+    // Assign first root to second root
     values[rootA] = rootB;
   }
 }
 
 function find(a, b) {
-  // travel up tree until find root for each
+  // Travel up tree until find root for each
   let rootA = values[a];
   let rootB = values[b];
 
-  // root occurs where value is equivalent to index
+  // Root occurs where value is equivalent to index
   while (rootA !== values[rootA]) {
     console.log('rootA: ', rootA);
     rootA = values[rootA];
@@ -30,7 +30,7 @@ function find(a, b) {
     rootB = values[rootB];
   }
 
-  // return comparison of roots as well as both roots for union call
+  // Return comparison of roots as well as both roots for union call
   return [rootA === rootB, rootA, rootB];
 }
 
