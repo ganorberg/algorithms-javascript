36ab0ee4412296edb0bbf496c606fe557cf888af
George Norberg <george.norberg@gmail.com>
2017-11-28 21:15:40 -0800

    Insert answers into logs and capitalize comments

--- selection/quick-select.js
+++ selection/quick-select.js
@@ -2,8 +2,8 @@ function partition(arr, lo, hi) {
   let i = lo;
   let j = hi + 1;
   while (true) {
-    while (arr[++i] < arr[lo] && i < hi) {}
-    while (arr[--j] > arr[lo] && j > lo) {}
+    while (arr[++i] < arr[lo] && i < hi) { }
+    while (arr[--j] > arr[lo] && j > lo) { }
     if (i >= j) { break; }
     [arr[i], arr[j]] = [arr[j], arr[i]];
   }
@@ -28,19 +28,21 @@ function quickSelect(arr, k) {
   while (true) {
     const pivotIndex = partition(arr, lo, hi);
 
-    // congratulations, you found kth smallest value!
+    // Congratulations, you found kth smallest value!
     if (k === pivotIndex) { return arr[k]; }
 
-    // if kth smallest value is in smaller partition, look there
+    // If kth smallest value is in smaller partition, look there
     if (k < pivotIndex) { hi = pivotIndex - 1; }
 
-    // if kth smallest value is in larger partition, look there
+    // If kth smallest value is in larger partition, look there
     if (k > pivotIndex) { lo = pivotIndex + 1; }
   }
 }
 
 const arr = [1, 4, 2, 3, 9, 1, 6];
 const arr2 = [7];
-console.log(quickSelect(arr, 5)); // 6
-console.log(quickSelect(arr, 2)); // 2
-console.log(quickSelect(arr2, 0)); // 7
+const arr3 = [7, 6, 10, 30, 1, 72, 80, 12];
+console.log(6, quickSelect(arr, 5));
+console.log(2, quickSelect(arr, 2));
+console.log(7, quickSelect(arr2, 0));
+console.log(10, quickSelect(arr3, 3)); 
