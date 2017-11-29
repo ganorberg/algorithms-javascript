2c1b6d0e37cb26f5e56cd4fd30d29d2d2f897741
George Norberg <george.norberg@gmail.com>
2017-11-28 20:15:50 -0800

    Check if a string is a permutation of a palindrome

---
+++ arrays-and-strings/palindrome-permutation.js
@@ -0,0 +1,7 @@
+/* Given a string, write a function to check if it is a permutation of a 
+ * palindrome.
+ * 
+ * STRATEGY
+ * Count all letters with hash map. Loop through letter counts, and if more than
+ * one is odd, return false. Otherwise return true.
+ */
