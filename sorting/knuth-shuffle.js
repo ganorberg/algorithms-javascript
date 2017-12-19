ba844729182e4aec049239ce9bf9ebf836bfc57c
George Norberg <george.norberg@gmail.com>
2017-12-19 13:57:51 -0800

    Implement correct efficient randomized shuffle

--- sorting/knuth-shuffle.js
+++ sorting/knuth-shuffle.js
@@ -1,16 +1,42 @@
-// unbiased random permutation in linear time. aka fisher-yates shuffle.
+// Unbiased random permutation in linear time (aka fisher-yates shuffle).
 
 function knuthShuffle(array) {
-  // loop through array
-  for (let i = 0; i < array.length; i++) {
-    // generate random index between 0 and current index (exclusive)
-    const random = Math.floor(Math.random() * i);
-
-    // swap current element with element at random index
+  // Skip last element because nothing left to swap with
+  for (let i = array.length - 1; i > 0; i--) {
+    // i + 1 means inclusive on current element, as opposed to i as exclusive
+    const random = Math.floor(Math.random() * (i + 1));
     [array[i], array[random]] = [array[random], array[i]];
   }
 }
 
 const arr = [3, 2, -4, 1, 6, 8, 0, 9, -5, -10, 12, -3, -100, -1000];
+knuthShuffle(arr);
+console.log(arr);
+knuthShuffle(arr);
+console.log(arr);
+knuthShuffle(arr);
+console.log(arr);
+knuthShuffle(arr);
+console.log(arr);
+knuthShuffle(arr);
+console.log(arr);
+knuthShuffle(arr);
+console.log(arr);
+knuthShuffle(arr);
+console.log(arr);
+knuthShuffle(arr);
+console.log(arr);
+knuthShuffle(arr);
+console.log(arr);
+knuthShuffle(arr);
+console.log(arr);
+knuthShuffle(arr);
+console.log(arr);
+knuthShuffle(arr);
+console.log(arr);
+knuthShuffle(arr);
+console.log(arr);
+knuthShuffle(arr);
+console.log(arr);
 knuthShuffle(arr);
 console.log(arr);
