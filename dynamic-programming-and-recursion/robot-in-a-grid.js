377e036899a3b2356ceca40eef424c1aa5f16520
George Norberg <george.norberg@gmail.com>
2017-12-19 20:51:28 -0800

    Show the robot its way home as efficiently as possible

--- dynamic-programming-and-recursion/robot-in-a-grid.js
+++ dynamic-programming-and-recursion/robot-in-a-grid.js
@@ -22,46 +22,109 @@
  * - O(2^(r+c)) without caching visits
  * - O(rc) by caching visits to avoid visiting cells whose paths have already been calculated
  */
-function getGridPath(grid, row = 0, column = 0, path = [], visited = new Set()) {
-  // Test for invalid inputs
+function getGridPath(grid) {
   if (
     !Array.isArray(grid)
     || grid.length === 0
     || grid.map(row => Array.isArray(row)).includes(false)
-    || row < 0
-    || column < 0
-    || !Number.isSafeInteger(row)
-    || !Number.isSafeInteger(column)
-    || !Array.isArray(path)
-    || Object.prototype.toString.call(visited) !== '[object Set]'
   ) {
     return 'Please insert valid inputs';
   }
 
-  const totalRows = grid.length;
-  const totalColumns = grid[0].length;
+  const ORIGIN_ROW = 0;
+  const ORIGIN_COLUMN = 0;
+  const path = [stringifyPosition(ORIGIN_ROW, ORIGIN_COLUMN)];
+  const visited = new Set();
 
-  // Base cases: out of bounds, hit an "off limit" cell, or already visited this cell
-  if (
-    row >= totalRows
-    || column >= totalColumns
-    || grid[row][column] === false
-    || visited.has(`${row},${column}`)
-  ) { return null; }
-
-  // Copy path to avoid all routes sharing same array
-  const localPath = [...path, [row, column]];
-
-  // Globally track history to avoid repeat visits and duplicate work
-  visited.add(`${row},${column}`);
-
-  // Base case: reached bottom right of grid
-  if (row === totalRows - 1 && column === totalColumns - 1) { return localPath; }
-  
-  // Move down or to the right in all possible combinations until hit base cases
-  return getGridPath(grid, row + 1, column, localPath, visited) ||
-         getGridPath(grid, row, column + 1, localPath, visited);
+  return getPath(grid, ORIGIN_ROW, ORIGIN_COLUMN, path, visited);
 }
+
+function stringifyPosition(row, column) {
+  return `${row},${column}`;
+}
+
+function getPath(grid, robotRow, robotColumn, path, visited) {
+  if (invalidMove(grid, robotRow, robotColumn, visited)) { return false; }
+  if (reachedDestination(grid, robotRow, robotColumn)) { return path; }
+  visited.add(stringifyPosition(robotRow, robotColumn));
+
+  // If calls fail to find path, backtrack so path array forgets those visits
+  const MOVE_DOWN = robotRow + 1;
+  path.push(stringifyPosition(MOVE_DOWN, robotColumn));
+  let isFinished = getPath(grid, MOVE_DOWN, robotColumn, path, visited);
+  if (isFinished) { return path; }
+  path.pop();
+
+  const MOVE_RIGHT = robotColumn + 1;
+  path.push(stringifyPosition(robotRow, MOVE_RIGHT));
+  isFinished = getPath(grid, robotRow, MOVE_RIGHT, path, visited);
+  if (isFinished) { return path; }
+  path.pop();
+}
+
+function outOfBounds(grid, robotRow, robotColumn) {
+  return robotRow >= grid.length || robotColumn >= grid[0].length;
+}
+
+function offLimitCell(grid, robotRow, robotColumn) {
+  return grid[robotRow][robotColumn] === false;
+}
+
+function alreadyVisited(grid, robotRow, robotColumn, visited) {
+  return visited.has(stringifyPosition(robotRow, robotColumn));
+}
+
+function invalidMove(grid, robotRow, robotColumn, visited) {
+  return outOfBounds(grid, robotRow, robotColumn)
+    || offLimitCell(grid, robotRow, robotColumn)
+    || alreadyVisited(grid, robotRow, robotColumn, visited);
+}
+
+function reachedDestination(grid, robotRow, robotColumn) {
+  return robotRow === grid.length - 1 && robotColumn === grid[0].length - 1;
+}
+
+// OLD INEFFICIENT UGLY SOLUTION
+// function getGridPath(grid, row = 0, column = 0, path = [], visited = new Set()) {
+//   // Test for invalid inputs
+//   if (
+//     !Array.isArray(grid)
+//     || grid.length === 0
+//     || grid.map(row => Array.isArray(row)).includes(false)
+//     || row < 0
+//     || column < 0
+//     || !Number.isSafeInteger(row)
+//     || !Number.isSafeInteger(column)
+//     || !Array.isArray(path)
+//     || Object.prototype.toString.call(visited) !== '[object Set]'
+//   ) {
+//     return 'Please insert valid inputs';
+//   }
+
+//   const totalRows = grid.length;
+//   const totalColumns = grid[0].length;
+
+//   // Base cases: out of bounds, hit an "off limit" cell, or already visited this cell
+//   if (
+//     row >= totalRows
+//     || column >= totalColumns
+//     || grid[row][column] === false
+//     || visited.has(`${row},${column}`)
+//   ) { return null; }
+
+//   // Copy path to avoid all routes sharing same array
+//   const localPath = [...path, [row, column]];
+
+//   // Globally track history to avoid repeat visits and duplicate work
+//   visited.add(`${row},${column}`);
+
+//   // Base case: reached bottom right of grid
+//   if (row === totalRows - 1 && column === totalColumns - 1) { return localPath; }
+
+//   // Move down or to the right in all possible combinations until hit base cases
+//   return getGridPath(grid, row + 1, column, localPath, visited) ||
+//     getGridPath(grid, row, column + 1, localPath, visited);
+// }
 
 const T = true;
 const F = false;
@@ -137,5 +200,5 @@ console.log(getGridPath(testGrid1));
 console.log(getGridPath(testGrid2));
 console.log(getGridPath(testGrid3));
 console.log(getGridPath([[T]]));
-console.log(getGridPath());
-console.log(getGridPath([{}]));
+console.log('should be invalid: ', getGridPath());
+console.log('should be invalid: ', getGridPath([{}]));
