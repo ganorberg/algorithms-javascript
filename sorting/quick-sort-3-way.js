e132f134534a070cfe3ee97eae801c8babb786e9
George Norberg <george.norberg@gmail.com>
2017-12-19 19:01:17 -0800

    Separate partition function

--- sorting/quick-sort-3-way.js
+++ sorting/quick-sort-3-way.js
@@ -16,50 +16,50 @@ function knuthShuffle(array) {
   }
 }
 
-function sort(arr, lo, hi) {
-  // Base case: when pointers cross, array is sorted!
-  if (hi <= lo) { return; }
+// function sort(arr, lo = 0, hi = arr.length - 1) {
+//   // Base case: when pointers cross, array is sorted!
+//   if (hi <= lo) { return; }
 
-  // Select pivot to be first item
-  let pivot = lo;
+//   // Select pivot to be first item
+//   let pivot = lo;
 
-  // Left pointer will directly compare to pivot and right for swapping
-  let left = lo + 1;
+//   // Left pointer will directly compare to pivot and right for swapping
+//   let left = lo + 1;
 
-  // Right pointer values are never directly compared to pivot, but are used
-  // to deliver values to left pointer for comparison with pivot and eventually
-  // end the loop when it crosses the left pointer
-  let right = hi;
+//   // Right pointer values are never directly compared to pivot, but are used
+//   // to deliver values to left pointer for comparison with pivot and eventually
+//   // end the loop when it crosses the left pointer
+//   let right = hi;
 
-  // Partition until pointers cross
-  while (left <= right) {
-    // Important part of 3-way partitioning is simply moving left pointer on dupes
-    if (arr[left] === arr[pivot]) { left++; }
+//   // Partition until pointers cross
+//   while (left <= right) {
+//     // Important part of 3-way partitioning is simply moving left pointer on dupes
+//     if (arr[left] === arr[pivot]) { left++; }
 
-    // Only swap with pivot when left pointer value is lesser
-    else if (arr[left] < arr[pivot]) {
-      swapInPlace(arr, left, pivot);
-      left++;
-      pivot++;
-    }
+//     // Only swap with pivot when left pointer value is lesser
+//     else if (arr[left] < arr[pivot]) {
+//       swapInPlace(arr, left, pivot);
+//       left++;
+//       pivot++;
+//     }
 
-    // Cannot swap with pivot, so swap left with right. This creates a region of 
-    // values greater than the pivot to the right of the right pointer.
-    else if (arr[left] > arr[pivot]) {
-      swapInPlace(arr, left, right);
-      right--;
-    }
-  }
+//     // Cannot swap with pivot, so swap left with right. This creates a region of 
+//     // values greater than the pivot to the right of the right pointer.
+//     else if (arr[left] > arr[pivot]) {
+//       swapInPlace(arr, left, right);
+//       right--;
+//     }
+//   }
 
-  // Pivot is sorted, so now sort left and right subarrays
-  sort(arr, lo, pivot - 1);
-  sort(arr, pivot + 1, hi);
-}
+//   // Pivot is sorted, so now sort left and right subarrays
+//   sort(arr, lo, pivot - 1);
+//   sort(arr, pivot + 1, hi);
+// }
 
 function quickSort(arr) {
-  // Randomization necessary to guarantee performance
+  // Randomize to guarantee performance
   knuthShuffle(arr);
-  sort(arr, 0, arr.length - 1);
+  sort(arr);
 }
 
 const str = 'KRATELEPUIMQDIFUHOA';
@@ -72,3 +72,29 @@ const arr2 = Array.from(str2);
 quickSort(arr2);
 console.log(arr2);
 
+function sort(arr, lo = 0, hi = arr.length - 1) {
+  if (hi <= lo) { return; }
+  let pivot = partition3Way(arr, lo, hi);
+  sort(arr, lo, pivot - 1);
+  sort(arr, pivot + 1, hi);
+}
+
+function partition3Way(arr, lo, hi) {
+  let pivot = lo;
+  let left = lo + 1;
+  let right = hi;
+
+  while (left <= right) {
+    if (arr[left] === arr[pivot]) { left++; }
+    else if (arr[left] < arr[pivot]) {
+      swapInPlace(arr, left, pivot);
+      left++;
+      pivot++;
+    } else {
+      swapInPlace(arr, left, right);
+      right--;
+    }
+  }
+
+  return right;
+}
