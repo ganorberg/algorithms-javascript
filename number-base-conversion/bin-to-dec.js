5125d03e1a2300429e0dcbfc26173e477ee35f48
George Norberg <george.norberg@gmail.com>
2018-02-03 20:21:57 -0800

    Use bits in binary instead of converting to string

--- number-base-conversion/bin-to-dec.js
+++ number-base-conversion/bin-to-dec.js
@@ -5,22 +5,37 @@ function binToDec(binary) {
 
 // Manually
 function binToDec(binary) {
-  // Make sure binary value is in string format for looping
-  const bin = String(binary);
+  let decimal = 0;
 
-  let dec = 0;
-  let power = 0;
-
-  for (let i = bin.length - 1; i >= 0; i--, power++) {
-    dec += (2 ** power) * Number(bin[i]);
+  for (let i = 0; i < 32; i++) {
+    const bit = getBit(binary, i);
+    decimal += (2 ** i) * bit;
   }
 
-  return dec;
+  return decimal;
 }
 
-console.log(0, binToDec('0'));
-console.log(3, binToDec('11'));
-console.log(4, binToDec('100'));
-console.log(5, binToDec('101'));
-console.log(5, binToDec('0101'));
-console.log(64, binToDec('1000000'));
+function getBit(binary, index) {
+  return Number((binary & (1 << index)) !== 0);
+}
+
+// Slow manually with binary string
+// function binToDec(binary) {
+//   // Make sure binary value is in string format for looping
+//   const bin = String(binary);
+
+//   let dec = 0;
+//   let power = 0;
+
+//   for (let i = bin.length - 1; i >= 0; i-- , power++) {
+//     dec += (2 ** power) * Number(bin[i]);
+//   }
+
+//   return dec;
+// }
+
+console.log(0, binToDec(0 & 0));
+console.log(3, binToDec(3 & 3));
+console.log(4, binToDec(4 & 4));
+console.log(5, binToDec(5 & 5));
+console.log(64, binToDec(64 & 64));
